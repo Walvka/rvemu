4e96a548bebd80fe561eb6e697b1aa1ccd7c4727
Walvka <2411082614@qq.com>
2023-05-23 18:02:29 +0800

    [3]初步实现模拟器框架

--- src/rvemu.c
+++ src/rvemu.c
@@ -6,7 +6,10 @@ int main(int argc, char *argv[]){
     machine_t machine = {0};
     machine_load_program(&machine, argv[1]);
     
-    printf("entry: 0x%llx\n", TO_HOST(machine.mmu.entry));
-    printf("host alloc: 0x%llx\n", TO_HOST(machine.mmu.host_alloc));
+    while (true){
+        enum exit_reason_t reason = machine_step(&machine);
+        assert(reason == ecall);
+    }
+    
     return 0;
 }
